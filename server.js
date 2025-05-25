501648ad4c60910928fb0bff860873d578487ced
dkmattiUCLL <r0801627@gmail.com>
2025-05-26 01:00:39 +0200

    :|

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 3000;
 app.get("/", (req, res) => {
   res.send("Hello from Docker!");
 });
-//
+
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
